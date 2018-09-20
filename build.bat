37c2ba14de7212846bba4c1c01966c4f34975733
tha2015 <tha2015@users.noreply.github.com>
2018-09-20 15:19:33 -0700

    update for eclipse 4.9

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call mvn package
-java -jar target/eclipse-dropins-builder-1.0-SNAPSHOT.jar src/main/resources/desktop_work4.xml
+java -jar target/eclipse-dropins-builder-1.0-SNAPSHOT.jar src/main/resources/desktop_work49.xml
